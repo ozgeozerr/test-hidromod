efc630b04a214e949c69b480a456eae7c242cbf4
ozgeozerr <ozerrrozge@gmail.com>
2026-01-28 14:07:25 +0300

    Move HTML files to public/, update Tailwind config for GitHub Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./**/*.{html,js}",
-  ],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'primary-blue': '#0066CC',
-        'primary-green': '#00A86B',
-        'light-blue': '#E6F2FF',
-        'light-green': '#E8F5F1',
+        "primary-blue": "#0066CC",
+        "primary-green": "#00A86B",
+        "light-blue": "#E6F2FF",
+        "light-green": "#E8F5F1",
       },
     },
   },
   plugins: [],
-}
+};
